7bf628e4e2bda7ab2cd34ecf18acde84e16661e7
ajit.shetty <ajit.shetty@trigyn.com>
2022-06-12 15:54:05 +0530

    .

--- implementations/cpims-tnt/update_system_settings.rb
+++ implementations/cpims-tnt/update_system_settings.rb
@@ -3,12 +3,13 @@
 def update_date_range_and_autopopulate_list
 	system_setting = SystemSettings.first
 	new_age_range  = [0..3, 4..6, 7..9, 10..13, 14..15, 16..17]
+
 	return if system_setting.age_ranges["primero"] == new_age_range
-	system_setting.age_ranges["primero"] = new_age_range
-	system_setting.age_ranges["unhcr"]   = new_age_range
-	new_autopopulate_list = {"field_key":"case_id_code","format":["registration_date.strftime('%Y/%m/%d')"],"separator":"-","auto_populated":true}
-	autopopulate_obj      = AutoPopulateInformation.new(new_autopopulate_list)
-  system_setting.auto_populate_list << autopopulate_obj 
+	system_setting.age_ranges         = { "primero" => new_age_range, "unhcr"=> new_age_range}
+	autopopulate_hash                 = {"field_key":"case_id_code","format":["registration_date.strftime('%Y/%m/%d')"],"separator":"-","auto_populated":true}
+	autopopulate_obj                  = AutoPopulateInformation.new(autopopulate_hash)
+  new_autopopulate_list             = system_setting.auto_populate_list + [autopopulate_obj]
+  system_setting.auto_populate_list = new_autopopulate_list
   system_setting.save
 end
 
