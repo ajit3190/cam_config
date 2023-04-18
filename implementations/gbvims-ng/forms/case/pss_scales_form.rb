74a0826e582d35b5701ba81ca0d254412a9629cc
ajit.shetty <ajit.shetty@trigyn.com>
2023-04-18 16:10:57 +0530

    [Update]

--- implementations/gbvims-ng/forms/case/pss_scales_form.rb
+++ implementations/gbvims-ng/forms/case/pss_scales_form.rb
@@ -81,7 +81,7 @@ FormSection.create_or_update!(
         date_validation: "default_date_validation",
         date_include_time: false,
         matchable: false,
-        mandatory_for_completion: false
+        mandatory_for_completion: false,
         collapsed_field_for_subform_unique_id: "pss_subform_section"
       },
       {
