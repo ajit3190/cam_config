e12cf92eeef8ebbd3940851ea38645501015ff34
ajit3190 <shettyajit9@gmail.com>
2024-08-13 20:01:04 +0530

    assets file

--- implementations/trigyn/theme/theme.rb
+++ implementations/trigyn/theme/theme.rb
@@ -28,11 +28,11 @@ theme.assign_attributes(
   }
 )
 
-theme.logo.attach(io: File.open("#{File.dirname(__FILE__)}/assets/logo.png"), filename: "logo.png")
-theme.logo_white.attach(io: File.open("#{File.dirname(__FILE__)}/assets/white-logo.png"), filename: "white-logo.png")
-theme.logo_pictorial_144.attach(io: File.open("#{File.dirname(__FILE__)}/assets/pictorial.png"), filename: "pictorial.png")
-theme.logo_pictorial_192.attach(io: File.open("#{File.dirname(__FILE__)}/assets/pictorial-192.png"), filename: "pictorial-192.png")
-theme.logo_pictorial_256.attach(io: File.open("#{File.dirname(__FILE__)}/assets/pictorial-256.png"), filename: "pictorial-256.png")
-theme.favicon.attach(io: File.open("#{File.dirname(__FILE__)}/assets/favicon.ico"), filename: "favicon.ico")
+theme.logo.attach(io: File.open("/srv/primero/cam_config/implementations/trigyn/theme/assets/primero-logo.png"), filename: "logo.png")
+theme.logo_white.attach(io: File.open("/srv/primero/cam_config/implementations/trigyn/theme/assets/white-logo.png"), filename: "white-logo.png")
+theme.logo_pictorial_144.attach(io: File.open("/srv/primero/cam_config/implementations/trigyn/theme/assets/tsfv_logo.png"), filename: "tsfv_logo.png")
+theme.logo_pictorial_192.attach(io: File.open("/srv/primero/cam_config/implementations/trigyn/theme/assets/pictorial-192.png"), filename: "pictorial-192.png")
+theme.logo_pictorial_256.attach(io: File.open("/srv/primero/cam_config/implementations/trigyn/theme/assets/pictorial-256.png"), filename: "pictorial-256.png")
+theme.favicon.attach(io: File.open("/srv/primero/cam_config/implementations/trigyn/theme/assets/favicon.ico"), filename: "favicon.ico")
 
 theme.save!
